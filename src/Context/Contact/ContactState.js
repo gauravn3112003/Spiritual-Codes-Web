cd955f0a907abae6b37c9153311be26de59c4126
Gaurav Narnaware <gauravnarnaware3112003@gmail.com>
2021-10-19 17:26:28 +0530

    Add blogs (db cnt) using thunderclient collection

--- src/Context/Contact/ContactState.js
+++ src/Context/Contact/ContactState.js
@@ -1,14 +1,28 @@
+import { useState } from "react/cjs/react.development";
 import ContactContext from "./contactContext";
 
 const ContactState = (props)=>{
 
-const addContact =  () => {
-  
-}
+    // fetch all blog data
 
+    const blogsInitial =[];
+    const [blogs, setBlogs] = useState(blogsInitial);
+    
+    const host = "http://localhost:5000";
+    const getBlog = async () => {
+      const res = await fetch(`${host}/api/createPost/fetchAllBlog`,{
+        method:"GET",
+        headers:{
+          "Content-Type":"application/json"
+        },
+      })
+      const json = await res.json();
+      console.log(json);
+      setBlogs(json)
+    };
 
     return(
-        <ContactContext.Provider value={addContact} >
+        <ContactContext.Provider value={{blogs,getBlog}} >
             {props.children}
         </ContactContext.Provider>
     )
