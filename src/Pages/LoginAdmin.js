ea6599637032d698ec3f607fccfb2478c9d6c51f
Gaurav Narnaware <gauravnarnaware3112003@gmail.com>
2021-10-23 15:41:16 +0530

    Improve in blog pages and login page

--- src/Pages/LoginAdmin.js
+++ src/Pages/LoginAdmin.js
@@ -1,6 +1,5 @@
 import React from "react";
-import  "../components/Styling/login.css"
-import { Link } from "react-router-dom";
+import "../components/Styling/login.css";
 export default function LoginAdmin() {
   return (
     <>
@@ -8,30 +7,42 @@ export default function LoginAdmin() {
         <div className="box">
           {/* <h1>Login</h1> */}
           <div className="login">
-            <form>
+            <form action="/Dashboard">
               <div className="img-login">
-                <img src="https://cdn.pixabay.com/photo/2015/11/26/00/14/woman-1063100__340.jpg" alt="" srcset="" />
+                <img
+                  src="https://cdn.pixabay.com/photo/2015/11/26/00/14/woman-1063100__340.jpg"
+                  alt=""
+                  srcset=""
+                />
               </div>
               <h2>Login</h2>
               <div className="input-box">
-                <div className="icon-login" >
+                <div className="icon-login">
                   <i class="bi bi-person-fill"></i>
                 </div>
                 <div className="input" data-aos="fade-down">
-                  <input type="email" className="input-login" placeholder="Enter the E-mail" />
+                  <input
+                    type="email"
+                    className="input-login"
+                    placeholder="Enter the E-mail"
+                  />
                 </div>
               </div>
 
               <div className="input-box">
-                <div className="icon-login" >
+                <div className="icon-login">
                   <i class="bi bi-key-fill"></i>
                 </div>
                 <div className="input" data-aos="fade-down">
-                  <input type="password" className="input-login"  placeholder="Enter the Password" />
+                  <input
+                    type="password"
+                    className="input-login"
+                    placeholder="Enter the Password"
+                  />
                 </div>
               </div>
               <div className="checkbox">
-                <input  type="checkbox" />
+                <input type="checkbox" />
                 Remember the password ?
               </div>
               <div className="btn">
@@ -41,7 +52,6 @@ export default function LoginAdmin() {
                   className="btn-sub"
                   value="Submit"
                 />
-                <Link to="/Dashboard">check</Link>
               </div>
             </form>
           </div>
--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -4,25 +4,30 @@ import { Link } from "react-router-dom";
 export default function BlogCard(props) {
   return (
     <div className="content card-3" data-aos="fade-down">
-      <img
-        src=
-        {props.image}
-        width="30px"
-        alt="project1"
-      />
-      <div className="tag">
-        #{props.tag}
-      </div>
+      <img src={props.image} width="30px" alt="project1" />
+      <div className="tag">#{props.tag}</div>
       <div className="head" data-aos="fade-down">
         {props.head}
       </div>
       <div className="text" data-aos="fade-down">
         {props.descri}
       </div>
+      <div className="date-author">
+        <div className="date">
+          <i class="bi bi-calendar-event"></i>Date:03/11/03{" "}
+        </div>
+        <div className="author">
+          <div className="icon-auth">
+          <i class="bi bi-person"></i>
+          </div>
+          <div className="text-auth">
+          Spirtual_codes
+          </div>
+        </div>
+      </div>
       <div className="btn" data-aos="fade-down">
-        <div className="date">Date: {props.date}</div>
         <div className="btn-1">
-          <Link to="/Blog/BlogOpen" > View</Link>
+          <Link to="/Blog/BlogOpen"> Read More</Link>
         </div>
       </div>
     </div>
